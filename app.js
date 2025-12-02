2baedd3215074802c5309c892342346363057a73
Chan2244 <booth.chandler@yahoo.com>
2025-12-01 17:04:24 -0700

    update

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
+
 require("dotenv").config();
 
-const expressLayouts = require("express-ejs-layouts")
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+const methodOverride = require("method-override");
+const session = require("express-session");
 
 const connectDB = require("./server/config/db");
 connectDB();
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.static("public"))
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+    // cookie: {maxAge:new Date(Date.now() +3600000)}
+    // Date.now() - 30 * 24 * 60 * 60 * 100
+  })
+);
 app.use(expressLayouts);
-app.use(express.static("public"))
-app.set("layout", "./layouts/main")
-app.set("view engine", "ejs")
+app.use(express.static("public"));
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
 
---
+++ server/routes/admin.js
@@ -0,0 +1,220 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
+const adminLayout = "../views/layouts/admin";
+
+/**
+ * Check Login Middleware
+ */
+const authMiddleweare = (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        req.userId = decoded.userId;
+        next();
+    }catch (error) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+};
+
+/**
+ * GET /
+ * Admin - Check Login
+ */
+router.get("/admin", async (req, res) => {
+    try{
+        const locals = {
+            title: "Admin",
+            description: "A blog template made with NodeJS and ExpressJS0,"
+        };
+
+        res.render("admin/index", { locals, layout: adminLayout });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * Post /login
+ * Admin - Login Account
+ */
+router.post("/admin", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const user = await User.findeOne({ username });
+
+        if (!user) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid Credentials" });
+        }
+
+        const token = jwt.sign({ userId: user._id}, jwtSecret);
+        res.cookie("token", token, { httpOnly: true});
+        res.redirect("/dashboard");
+    }catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * GET /dashboard
+ * Admin - Dashboard
+ */
+router.get("/dashboard", authMiddleweare, async (req, res) =>{
+    try {
+        const locals = {
+            title: "Dashboard",
+            description: "A blog template made with NodeJS and ExpressJS",
+        };
+
+        const data = await Post.find();
+        res.render("admin/dashboard", { locals, data, layout: adminLayout });
+    } catch (error) {}
+});
+
+/**
+ * POST /register
+ * Admin - Register Account
+ */
+router.post("/register", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        try {
+            const user = await User.create({
+                username, 
+                password: hashedPassword,
+            });
+            res.status(201).json({ message: "User created successfully", user });
+        } catch (error) {
+            if (error === 11000) {
+                return  res.status(500).json({ message: "User already Exists!"});
+            } else {
+                return res
+                .status(500)
+                .json({ message: "Something went wrong with your server!"});
+            }
+        }
+    }catch {
+        console.log(error);
+    }
+});
+
+/**
+ * GET /logout
+ * Admin - Logout
+ */
+router.get("/ogout", authMiddleweare, async (req, res) => {
+    try {
+        res.clearCookie("token");
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * GET /add-post
+ * Admin - Create New Post
+ */
+router.get("/add-post", authMiddleweare, async (req, ress) => {
+    try {
+        const locals = {
+            title: "Create Post",
+            description: "A blog template made with NodeJS and ExpressJS, and EJS",
+        };
+
+        const data = await Post.find();
+        res.render("admin/add-post", { locals, data, layout: adminLayout });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * POST /add-post
+ * Admin - Create New Post
+ */
+router.post("/add-post/", authMiddleweare, async (req, res) => {
+    try {
+        console.log(req.body);
+        try{
+            const newPost= new Post({
+                title: req.body.title,
+                body: req.body.body,
+            });
+            await Post.create(newPost);
+            res.redirect("/dashboard");
+        } catch (error) {
+            console.log(error);
+        }
+    }catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ *  Get /edit-post
+ * Admin - Update Post
+ */
+router.get("/edit-post/:id", authMiddleweare, async (req, res) =>{
+    try {
+        const locals = {
+            title: "Edit Post",
+            description: "A blog template made with NodeJS and ExpressJS, and EJS",
+        };
+
+        const data = await Post.findOne({ _id: req.params.id });
+        res.render("admin/edit-post", { locals, data, layot: adminLayout });
+    } catch (error) {
+        console.log(error);
+    } 
+});
+
+/**
+ * PUT /edit-post
+ * Admin - Edit Post
+ */
+router.put("/edit-post/:id", authMiddleweare, async (req, res) => {
+    try {
+        await Post.findByIdAndUpdate(req.params.id, {
+            title: req.body.title,
+            body: req.body.body,
+            updatedAt: Date.now(),
+        });
+        res.redirect(`/dashboard`);
+    }catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * DELETE /delete-post
+ * Admin - Delete Post
+ */
+router.delete("/delete-post/:id", authMiddleweare, async (req, res) => {
+    try {
+        await Post.deleteOne({ _id: req.params.id });
+        res.redirect("/dashboard");
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+module.exports = router;
